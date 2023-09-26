eb4bd793d4e9ddd34e19f63b116e83dacf0627e0
Santiago Lopez <santiagodavidlopezbenavides@gmail.com>
2023-09-26 08:17:09 -0500

    fix: delete modal

--- app/dashboard/center.tsx
+++ app/dashboard/center.tsx
@@ -1,20 +1,9 @@
 "use client";
 
-import {
-  Input,
-  Button,
-  Modal,
-  ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  useDisclosure,
-} from "@nextui-org/react";
+import { Input, Button } from "@nextui-org/react";
 import { useState } from "react";
 
 export default function Center() {
-  const { isOpen, onOpen, onOpenChange } = useDisclosure();
-
   const [data, setData] = useState({
     name: "",
     address: "",
@@ -59,7 +48,6 @@ export default function Center() {
           }));
         });
       } else {
-        onOpen();
         setErrors((prev) => ({ ...prev, name: body.error.message }));
       }
     } else {
@@ -68,124 +56,100 @@ export default function Center() {
   };
 
   return (
-    <>
-      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-        <Input
-          isRequired
-          label="Nombre"
-          placeholder="Ingrese el nombre"
-          type="text"
-          value={data.name}
-          onChange={(e) => {
-            setErrors((prev) => ({ ...prev, name: "" }));
-            setData({ ...data, name: e.target.value });
-          }}
-          isInvalid={Boolean(errors.name)}
-          errorMessage={errors.name}
-        />
-        <Input
-          isRequired
-          label="Dirección"
-          placeholder="Ingrese la dirección"
-          type="text"
-          value={data.address}
-          onChange={(e) => {
-            setErrors((prev) => ({ ...prev, address: "" }));
-            setData({ ...data, address: e.target.value });
-          }}
-          isInvalid={Boolean(errors.address)}
-          errorMessage={errors.address}
-        />
-        <Input
-          isRequired
-          label="Ciudad"
-          placeholder="Ingrese la ciudad"
-          type="text"
-          value={data.city}
-          onChange={(e) => {
-            setErrors((prev) => ({ ...prev, city: "" }));
-            setData({ ...data, city: e.target.value });
-          }}
-          isInvalid={Boolean(errors.city)}
-          errorMessage={errors.city}
-        />
-        <Input
-          isRequired
-          type="text"
-          label="Teléfono"
-          placeholder="Ingrese el teléfono"
-          startContent={
-            <div className="pointer-events-none flex items-center">
-              <span className="text-default-400 text-small">+57</span>
-            </div>
-          }
-          value={data.phone}
-          onChange={(e) => {
-            setErrors((prev) => ({ ...prev, phone: "" }));
-            setData({ ...data, phone: e.target.value });
-          }}
-          isInvalid={Boolean(errors.phone)}
-          errorMessage={errors.phone}
-        />
-        <Input
-          isRequired
-          label="Correo electrónico"
-          placeholder="Ingrese el correo electrónico"
-          type="email"
-          labelPlacement="inside"
-          value={data.email}
-          onChange={(e) => {
-            setErrors((prev) => ({ ...prev, email: "" }));
-            setData({ ...data, email: e.target.value });
-          }}
-          isInvalid={Boolean(errors.email)}
-          errorMessage={errors.email}
-        />
-
-        <div className="flex gap-2 justify-end">
-          <Button
-            fullWidth
-            color="primary"
-            type="submit"
-            isDisabled={
-              !data.name ||
-              !data.address ||
-              !data.city ||
-              !data.phone ||
-              !data.email ||
-              Boolean(errors.name) ||
-              Boolean(errors.address) ||
-              Boolean(errors.city) ||
-              Boolean(errors.phone) ||
-              Boolean(errors.email)
-            }
-          >
-            Crear Centro
-          </Button>
-        </div>
-      </form>
-      <Modal
-        isOpen={isOpen}
-        onOpenChange={onOpenChange}
-        isDismissable={false}
-        scrollBehavior="outside"
-        placement="bottom"
-        backdrop="transparent"
-        classNames={{
-          base: "bg-primary text-foreground",
-          closeButton: "text-white hover:bg-[#66aaf9]",
+    <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+      <Input
+        isRequired
+        label="Nombre"
+        placeholder="Ingrese el nombre"
+        type="text"
+        value={data.name}
+        onChange={(e) => {
+          setErrors((prev) => ({ ...prev, name: "" }));
+          setData({ ...data, name: e.target.value });
         }}
-      >
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1">
-                Centro creado exitosamente
-              </ModalHeader>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
-    </>
+        isInvalid={Boolean(errors.name)}
+        errorMessage={errors.name}
+      />
+      <Input
+        isRequired
+        label="Dirección"
+        placeholder="Ingrese la dirección"
+        type="text"
+        value={data.address}
+        onChange={(e) => {
+          setErrors((prev) => ({ ...prev, address: "" }));
+          setData({ ...data, address: e.target.value });
+        }}
+        isInvalid={Boolean(errors.address)}
+        errorMessage={errors.address}
+      />
+      <Input
+        isRequired
+        label="Ciudad"
+        placeholder="Ingrese la ciudad"
+        type="text"
+        value={data.city}
+        onChange={(e) => {
+          setErrors((prev) => ({ ...prev, city: "" }));
+          setData({ ...data, city: e.target.value });
+        }}
+        isInvalid={Boolean(errors.city)}
+        errorMessage={errors.city}
+      />
+      <Input
+        isRequired
+        type="text"
+        label="Teléfono"
+        placeholder="Ingrese el teléfono"
+        startContent={
+          <div className="pointer-events-none flex items-center">
+            <span className="text-default-400 text-small">+57</span>
+          </div>
+        }
+        value={data.phone}
+        onChange={(e) => {
+          setErrors((prev) => ({ ...prev, phone: "" }));
+          setData({ ...data, phone: e.target.value });
+        }}
+        isInvalid={Boolean(errors.phone)}
+        errorMessage={errors.phone}
+      />
+      <Input
+        isRequired
+        label="Correo electrónico"
+        placeholder="Ingrese el correo electrónico"
+        type="email"
+        labelPlacement="inside"
+        value={data.email}
+        onChange={(e) => {
+          setErrors((prev) => ({ ...prev, email: "" }));
+          setData({ ...data, email: e.target.value });
+        }}
+        isInvalid={Boolean(errors.email)}
+        errorMessage={errors.email}
+      />
+
+      <div className="flex gap-2 justify-end">
+        <Button
+          fullWidth
+          color="primary"
+          type="submit"
+          isDisabled={
+            !data.name ||
+            !data.address ||
+            !data.city ||
+            !data.phone ||
+            !data.email ||
+            Boolean(errors.name) ||
+            Boolean(errors.address) ||
+            Boolean(errors.city) ||
+            Boolean(errors.phone) ||
+            Boolean(errors.email)
+          }
+        >
+          Crear Centro
+        </Button>
+      </div>
+    </form>
   );
 }
